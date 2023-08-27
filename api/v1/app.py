4fd539f798b22bb58cfc965da23780c647f82892
Franco <francorr240@gmail.com>
2023-08-27 06:56:09 -0300

    Task 6 complete

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Creates flask app """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 app = Flask(__name__)
@@ -10,6 +10,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def fourofour(exception):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
